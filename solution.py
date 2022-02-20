81bae447f2788054d61f5bd0589a49ee5afeb397
hailzno <98563532+hailzno@users.noreply.github.com>
2022-02-19 23:08:17 -0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,48 +2,35 @@
 from socket import *
 # In order to terminate the program
 import sys
-
-
 def webServer(port=13331):
     serverSocket = socket(AF_INET, SOCK_STREAM)
     # Prepare a server socket
     #Assign the port number
     serverSocket.bind(("", port))
     serverSocket.listen(1)
-
     while True:
         # Establish the connection
-            print('Ready to serve...')
-            connectionSocket, addr = serverSocket.accept()
-
+        connectionSocket, addr = serverSocket.accept()
+        try:
             try:
                 message = connectionSocket.recv(1024)
                 filename = message.split()[1]
-                print('Open File: ', filename)
                 f = open(filename[1:])
                 outputdata = f.read()
-                print(outputdata)
                 # Send one HTTP header line into socket.
-                connectionSocket.send("\nHTTP/1.1 200 OK\n")
-                connectionSocket.send(outputdata)
-
+                connectionSocket.send('HTTP/1.1 200 OK\r\n'.encode())
                 # Send the content of the requested file to the client
                 for i in range(0, len(outputdata)):
                     connectionSocket.send(outputdata[i].encode())
-                    connectionSocket.send("\r\n".encode())
-
+                connectionSocket.send("\r\n".encode())
                 connectionSocket.close()
-
             except IOError:
              # Send response message for file not found (404)
-                connectionSocket.send("\nHTTP/1.1 404 Not Found\n")
-
+                connectionSocket.send('HTTP/1.1 404 Not Found\r\n'.encode())
                 connectionSocket.close()
-
-
+        except (ConnectionResetError, BrokenPipeError):
+            pass
     serverSocket.close()
     sys.exit()  # Terminate the program after sending the corresponding data
-
-
 if __name__ == "__main__":
     webServer(13331)
